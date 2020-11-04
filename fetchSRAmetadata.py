bbf4e5716413d5733a411765379cb35b112f7cb7
Muhammad Zohaib Anwar <mzanwar@envs.au.dk>
2020-11-04 13:34:59 -0800

    Update fetchSRAmetadata.py

--- fetchSRAmetadata.py
+++ fetchSRAmetadata.py
@@ -8,10 +8,10 @@ db = SRAweb()
 
 # Reading the most recent file of NCBI search based on organism
 # taxonomy (07 July, 2020)
-SRA = pd.read_csv("COVID_SRA_07July.csv")
+SRA = pd.read_csv("Coronaviridae_runs_20201103.csv")
 
 # Extracting distinct studies to reduce the search space
-studies = SRA['sra_study'].unique()
+studies = SRA['sra_study'].nunique()
 pd_data = []
 for srp in sorted(studies.tolist()):
     try:
@@ -27,4 +27,4 @@ for srp in sorted(studies.tolist()):
 final_pd = pd.concat(pd_data, sort=False)
 
 # Final DataFrame written to CSV file
-final_pd.to_csv("COVID_Metadata_detailed.tsv", sep="\t", index=False)
+final_pd.to_csv("COVID_Metadata_detailed_20201103.tsv", sep="\t", index=False)
